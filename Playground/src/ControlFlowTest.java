86983ca6a1c16e8d069240ca3b3f89077ce0e85f
joeyjira <joey@Julia.hsd1.ca.comcast.net>
2018-01-14 22:06:03 -0800

    salary flow test

--- Playground/src/ControlFlowTest.java
+++ Playground/src/ControlFlowTest.java
@@ -1,12 +1,34 @@
+import java.util.*;
 
 public class ControlFlowTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter employee name: ");
+		String name = in.nextLine();
+		
+		System.out.println("Enter employee sales number: ");
+		int salesNumber = in.nextInt();
+		
+		if (salesNumber > 1000)
 		{
-			int k;
+			String performance = "Excellent";
+			int bonus = 1000;			
 		}
+		
+		System.out.println("Confirm employee name and salary: y/n");
+		String confirmation = in.next();
+		
+		if (confirmation.equals("y"))
+		{
+			System.out.println("Data input confirmed");
+		}
+		else 
+		{
+			System.out.println("Restart program");
+		}
+//		System.out.println(0x12);
 	}
 
 }
