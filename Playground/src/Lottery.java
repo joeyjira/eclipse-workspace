fa331ba0414b48c458c32cab4b5b082eb111b161
joeyjira <joey.jira@gmail.com>
2018-01-16 13:57:36 -0800

    big number test

--- Playground/src/Lottery.java
+++ Playground/src/Lottery.java
@@ -1,9 +1,24 @@
+import java.math.*;
+import java.util.*;
 
 public class Lottery {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("How many numbers do you need to draw? ");
+		int k = in.nextInt();
+		
+		System.out.print("What is the highest number you can draw? ");
+		int n = in.nextInt();
+		
+		BigInteger lotteryOdds = BigInteger.valueOf(1);
+		
+		for(int i = 1; i <= k; i++)
+			lotteryOdds = lotteryOdds.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
+		
+		System.out.println("Your odds are 1 in " + lotteryOdds + ". Good luck!");
 	}
 
 }
