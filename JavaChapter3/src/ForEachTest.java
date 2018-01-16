e2c54add4ba8eb30e2534b51a90a1f9fbc10716f
joeyjira <joey.jira@gmail.com>
2018-01-16 14:29:19 -0800

    for each loop

---
+++ JavaChapter3/src/ForEachTest.java
@@ -0,0 +1,15 @@
+
+public class ForEachTest {
+	public static void main(String[] args)
+	{
+		int[] a = new int[10];
+		int counter = 0;
+		
+		for (int element: a)
+		{
+			counter += 3;
+			element = counter;
+			System.out.println(element);
+		}
+	}
+}
