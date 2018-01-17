f99bfae4c1bdebb84ee2319f455737ba24ff7c1c
joeyjira <joey.jira@gmail.com>
2018-01-17 11:28:10 -0800

    compound interest

--- JavaChapter3/src/ArraysTest.java
+++ JavaChapter3/src/ArraysTest.java
@@ -1,7 +1,8 @@
 
 public class ArraysTest {
 	public static void main(String[] args) {
-		int[] a = new int[10];
+		int[] a;
+		a = new int[10];
 		for (int i = 0; i < a.length; i++)
 		{
 			a[i] = i;
