d3f9c54e56bd0f0146de19dcdcbc2ee54a676c1a
joeyjira <joey.jira@gmail.com>
2018-01-19 15:53:37 -0800

    split employee class and test to 2 files

---
+++ JavaChapter4/src/Employee.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+
+class Employee
+{
+	private String name;
+	private double salary;
+	private LocalDate hireDay;
+	
+	public static void main(String[] args)
+	{
+		System.out.println();
+	}
+	
+	public Employee(String n, double s, int year, int month, int day)
+	{
+		name = n;
+		salary = s;
+		hireDay = LocalDate.of(year, month, day);		
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getSalary()
+	{
+		return salary;
+	}
+	
+	public LocalDate getHireDay()
+	{
+		return hireDay;
+	}
+	
+	public void raiseSalary(double byPercent)
+	{
+		double raise = salary * byPercent/100;
+		salary += raise;
+	}
+}
--- JavaChapter4/src/EmployeeTest.java
+++ JavaChapter4/src/EmployeeTest.java
@@ -1,4 +1,3 @@
-import java.time.LocalDate;
 
 public class EmployeeTest {
 	public static void main(String[] args)
@@ -20,37 +19,4 @@ public class EmployeeTest {
 	}
 }
 
-class Employee
-{
-	private String name;
-	private double salary;
-	private LocalDate hireDay;
-	
-	public Employee(String n, double s, int year, int month, int day)
-	{
-		name = n;
-		salary = s;
-		hireDay = LocalDate.of(year, month, day);		
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
-	public double getSalary()
-	{
-		return salary;
-	}
-	
-	public LocalDate getHireDay()
-	{
-		return hireDay;
-	}
-	
-	public void raiseSalary(double byPercent)
-	{
-		double raise = salary * byPercent/100;
-		salary += raise;
-	}
-}
+
