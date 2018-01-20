899aa45b433ed36a447534ca22c38d9d028aab0b
joeyjira <joey.jira@gmail.com>
2018-01-19 21:07:10 -0800

    overloading test

--- JavaChapter4/src/Employee.java
+++ JavaChapter4/src/Employee.java
@@ -2,20 +2,26 @@ import java.time.LocalDate;
 
 class Employee
 {
+	private static int nextId = 1;
+	
 	private String name;
 	private double salary;
 	private LocalDate hireDay;
 	
-	public static void main(String[] args)
+	private int id;
+	
+	public Employee(String n, double s)
 	{
-		System.out.println();
+		name = n;
+		salary = s;
+		id = 0;	
 	}
 	
 	public Employee(String n, double s, int year, int month, int day)
 	{
 		name = n;
 		salary = s;
-		hireDay = LocalDate.of(year, month, day);		
+		hireDay = LocalDate.of(year, month, day);
 	}
 	
 	public String getName()
@@ -33,9 +39,31 @@ class Employee
 		return hireDay;
 	}
 	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId()
+	{
+		id = nextId; // set id to next available id
+		nextId++;
+	}
+	
+	public static int getNextId()
+	{
+		return nextId; // returns static field
+	}
+	
 	public void raiseSalary(double byPercent)
 	{
 		double raise = salary * byPercent/100;
 		salary += raise;
+	}
+	
+	public static void main(String[] args)
+	{
+		Employee e = new Employee("Harry", 50000);
+		System.out.println(e.getName() + " " + e.getSalary());
 	}
 }
