e2d39aaf938a2437abf97492ecf13d51369dc20b
joeyjira <joey.jira@gmail.com>
2018-01-21 14:41:50 -0800

    4 constructors test

--- JavaChapter4/src/Employee.java
+++ JavaChapter4/src/Employee.java
@@ -1,19 +1,34 @@
 import java.time.LocalDate;
+import java.util.Random;
 
 class Employee
 {
 	private static int nextId;
 	
-	private String name;
+	private int id;
+	private String name = ""; // instance field initialization
 	private double salary;
 	private LocalDate hireDay;
-	private int id;
 	
+	// static initialization block
+	static
+	{
+		Random generator = new Random();
+		// set nextId to a random number between 0 and 9999
+		nextId = generator.nextInt(10000);
+	}
+	
+	// object initialization block
+	{
+		id = nextId;
+		nextId++;
+	}
+	
+	// four overloaded constructors
 	public Employee(String n, double s)
 	{
 		name = n;
 		salary = s;
-		id = 0;	
 	}
 	
 	public Employee(String n, double s, int year, int month, int day)
@@ -21,6 +36,20 @@ class Employee
 		name = n;
 		salary = s;
 		hireDay = LocalDate.of(year, month, day);
+	}
+	
+	public Employee(double s)
+	{
+		// calls the Employee(String, double) constructor
+		this("Employee #" + nextId, s);
+	}
+	
+	// default constructor
+	public Employee()
+	{
+		// name initialized to "" --see above
+		// salary not explicitly set--initialized to 0
+		// id initialized in initialization block
 	}
 	
 	public String getName()
@@ -33,36 +62,19 @@ class Employee
 		return salary;
 	}
 	
-	public LocalDate getHireDay()
-	{
-		return hireDay;
-	}
-	
 	public int getId()
 	{
 		return id;
 	}
 	
-	public void setId()
+	public LocalDate getHireDay()
 	{
-		id = nextId; // set id to next available id
-		nextId++;
+		return hireDay;
 	}
 	
-	public static int getNextId()
+	public void raiseSalary(double percentage) 
 	{
-		return nextId; // returns static field
-	}
-	
-	public void raiseSalary(double byPercent)
-	{
-		double raise = salary * byPercent/100;
+		double raise = salary * ((100 + percentage) / 100); 
 		salary += raise;
-	}
-	
-	public static void main(String[] args)
-	{
-		Employee e = new Employee("Harry", 50000);
-		System.out.println(e.getName() + " " + e.getSalary());
 	}
 }
