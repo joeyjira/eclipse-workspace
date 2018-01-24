ca30cdfd7e00890ee9920e7f94573370869a6859
joeyjira <joey.jira@gmail.com>
2018-01-24 15:33:19 -0800

    equality test

---
+++ JavaChapter5/src/EqualityTest.java
@@ -0,0 +1,15 @@
+import abstractClasses.*;
+
+public class EqualityTest 
+{
+	public static void main(String[] args)
+	{
+		Person[] people = new Person[4];
+		
+		people[0] = new Employee("Harry Hacker", 100000, 2018, 3, 1);
+		people[1] = new abstractClasses.Student("Carry Cracker", "Literature");
+		people[2] = new Employee("Harry Hacker", 100000, 2018, 3, 1);
+		
+		System.out.println(people[0].equals(people[2]));
+	}
+}
