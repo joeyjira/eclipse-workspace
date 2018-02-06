d6ad726c97b808a642277d3cada642baf89b82a5
joeyjira <joey.jira@gmail.com>
2018-02-05 23:26:33 -0800

    general purpose test class

---
+++ Testing/src/Testing.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Testing {
+	public static void main(String[] args)
+	{
+		LocalDate deadline;
+		deadline = LocalDate.now();
+//		deadline = null;
+		
+		System.out.print(deadline.toString());
+		
+	}
+}
