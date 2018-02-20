c3527c5b31aa17c33a0fb0e4976893d1559e0c05
joeyjira <joey.jira@gmail.com>
2018-02-19 23:43:16 -0800

    timed greeter

---
+++ JavaChapter6/src/methodReference/Greeter.java
@@ -0,0 +1,12 @@
+package methodReference;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Greeter
+{
+	public void greet()
+	{
+		System.out.println("Hello, world!");
+	}
+}
---
+++ JavaChapter6/src/methodReference/TimedGreeter.java
@@ -0,0 +1,17 @@
+package methodReference;
+
+import javax.swing.Timer;
+
+public class TimedGreeter extends Greeter
+{
+	public void greet()
+	{
+		Timer t = new Timer(1000, super::greet);
+		t.start();
+	}
+	
+	public static void main(String[] args)
+	{
+		new TimedGreeter().greet();
+	}
+}
