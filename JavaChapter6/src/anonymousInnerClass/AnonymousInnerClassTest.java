9465f97e03709e1c8d57beb3c0ff3dba05df3127
joeyjira <joey.jira@gmail.com>
2018-02-23 18:29:13 -0800

    anonymous class test init

---
+++ JavaChapter6/src/anonymousInnerClass/AnonymousInnerClassTest.java
@@ -0,0 +1,17 @@
+package anonymousInnerClass;
+
+public class AnonymousInnerClassTest 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
+
+class TalkingClock
+{
+	public void start()
+	{
+		
+	}
+}
